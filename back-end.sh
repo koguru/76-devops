f6a931e19c6c71fd6675716a135e753032bfcdb4
koguru <k.ansar204@gmail.com>
2023-11-13 17:40:05 +0530

    updated bakend.sh file with my sql server address

--- back-end.sh
+++ back-end.sh
@@ -13,4 +13,4 @@ systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
 dnf install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.40.22 -uroot -pExpenseApp@1 < /app/schema/backend.sql
