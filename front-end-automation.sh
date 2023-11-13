a66c290bd8aafa3bba2a0a1ee678661b7a4f6280
koguru <k.ansar204@gmail.com>
2023-11-13 14:43:39 +0530

    modified

--- front-end-automation.sh
+++ front-end-automation.sh
@@ -6,5 +6,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp expense.conf /etc/nginx/default.d/
+
+cp /home/centos/76-devops/expense.conf /etc/nginx/default.d/expense.conf
 systemctl restart nginx
