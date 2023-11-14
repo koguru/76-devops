d9c8b6d38224696f75ad38d584e56ae7b9554a53
koguru <k.ansar204@gmail.com>
2023-11-14 13:02:08 +0530

    Adding output redirectors 1

--- front-end-automation.sh
+++ front-end-automation.sh
@@ -11,6 +11,6 @@ rm -rf /usr/share/nginx/html/*
 echo -e "\e[36m Download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/expense.log
 echo -e "\e[36m Restrting Nginx ngninx \e[0m"
 systemctl restart nginx &>>/tmp/expense.log
