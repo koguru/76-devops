5b7f53ff809c6239666f43d2a360a2242f095efb
koguru <k.ansar204@gmail.com>
2023-11-14 12:57:53 +0530

    Adding output redirectors

--- front-end-automation.sh
+++ front-end-automation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[36m Installing ngninx \e[0m"
+dnf install nginx -y &>>/tmp/expense.log
+echo -e "\e[36m Enabling  ngninx \e[0m"
+systemctl enable nginx &>>/tmp/expense.log
+echo -e "\e[36m Starting ngninx \e[0m"
+systemctl start nginx &>>/tmp/expense.log
 cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m Download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-systemctl restart nginx
+echo -e "\e[36m Restrting Nginx ngninx \e[0m"
+systemctl restart nginx &>>/tmp/expense.log
